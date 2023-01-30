77dcc49e1c513ffe1c826392db396d8fbfa056e0
2Dan3 <dan23.ftn@gmail.com>
2023-01-30 15:42:30 +0100

    Add: Circuit breaker in profile service

--- ProfileService/go.mod
+++ ProfileService/go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/golang-jwt/jwt/v4 v4.4.3
+	github.com/sony/gobreaker v0.5.0
 	go.mongodb.org/mongo-driver v1.11.0
 	google.golang.org/grpc v1.50.1
 	google.golang.org/protobuf v1.28.1
--- ProfileService/handlers/handlers.go
+++ ProfileService/handlers/handlers.go
@@ -6,8 +6,10 @@ import (
 	"ProfileService/proto/profile"
 	social "ProfileService/proto/social"
 	"context"
+	"github.com/sony/gobreaker"
 	"log"
 	"net/http"
+	"time"
 )
 
 type ProfileHandler struct {
@@ -27,6 +29,9 @@ func NewProfileHandler(l *log.Logger, repo *data.ProfileRepo, as auth.AuthServic
 	}
 }
 func (pr *ProfileHandler) Register(ctx context.Context, r *profile.ProfileRegisterRequest) (*profile.ProfileRegisterResponse, error) {
+
+	cb := pr.CircuitBreaker()
+
 	pr.l.Println("Profile service - Register")
 	user := &data.User{
 		Username:       r.Username,
@@ -41,19 +46,37 @@ func (pr *ProfileHandler) Register(ctx context.Context, r *profile.ProfileRegist
 		Private:        true,
 		Role:           r.Role,
 	}
-	err := pr.repo.Register(user)
-	if err != nil {
-		pr.l.Println("Error inserting user into db")
-		return nil, err
-	}
 
-	_, err6 := pr.ss.RegUser(context.Background(), &social.RegUserRequest{
-		Username: r.Username,
+	// C BREAKER
+
+	//bodyBytes, errCB := cb.Execute(func() (interface{}, error) {
+	_, errCB := cb.Execute(func() (interface{}, error) {
+
+		resp, err6 := pr.ss.RegUser(context.Background(), &social.RegUserRequest{
+			Username: r.Username,
+		})
+		if err6 != nil {
+			return nil, err6
+		}
+		// ?
+		pr.l.Println("response of rpc to social service: ", resp)
+		return resp, nil
 	})
-	if err6 != nil {
-		return nil, err6
+	if errCB != nil {
+		pr.l.Println("\n------------", errCB, "\n--------------")
+		// ?
+		return nil, errCB
+		//continue
 	}
 
+	errMongoReg := pr.repo.Register(user)
+	if errMongoReg != nil {
+		pr.l.Println("Error inserting user into db")
+		return nil, errMongoReg
+	}
+	//pr.l.Println("Body bytes: " + string(bodyBytes.([]byte)))
+
+	// C BREAKER
 	return &profile.ProfileRegisterResponse{}, nil
 }
 
@@ -102,4 +125,37 @@ func (pr *ProfileHandler) ManagePrivacy(ctx context.Context, r *profile.ManagePr
 		Privacy: r.Privacy,
 		Status:  http.StatusOK,
 	}, nil
+}
+
+func (pr *ProfileHandler) CircuitBreaker() *gobreaker.CircuitBreaker {
+	return gobreaker.NewCircuitBreaker(
+		gobreaker.Settings{
+			// circuit breaker's name
+			Name: "cb",
+			// maximum number of requests allowed to pass through when the circuit breaker is half-open
+			MaxRequests: 1,
+			// period of the open state, after which the state of the circuit breaker becomes half-open
+			// if Timeout is 0, the timeout value of CircuitBreaker is set to 60 seconds
+			Timeout: 2 * time.Second,
+			// cyclic period of the closed state for the circuit breaker to clear the internal Counts
+			// if Interval is 0, CircuitBreaker doesn't clear the internal Counts during the closed state
+			Interval: 0,
+			// called whenever a request fails in the closed state
+			// the circuit breaker will come into the open state if this function returns true
+			// type Counts struct {
+			//    Requests             uint32
+			//    TotalSuccesses       uint32
+			//    TotalFailures        uint32
+			//    ConsecutiveSuccesses uint32
+			//    ConsecutiveFailures  uint32
+			// }
+			ReadyToTrip: func(counts gobreaker.Counts) bool {
+				return counts.ConsecutiveFailures > 0
+			},
+			// called whenever the state of the circuit breaker changes
+			OnStateChange: func(name string, from gobreaker.State, to gobreaker.State) {
+				pr.l.Printf("Circuit Breaker '%s' changed from '%s' to '%s'\n", name, from, to)
+			},
+		},
+	)
 }
